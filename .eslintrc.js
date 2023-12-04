ecc6c33ad0d865a86e415322a55bb69ccb2ced59
Tim de Booij <tim.debooij@9to5.software>
2023-12-04 17:07:18 +0100

    fix: parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,22 @@
 "use strict";
 
-module.exports = {
-  root: true,
-  extends: [
-    "eslint:recommended",
-    "plugin:eslint-plugin/recommended",
-    "plugin:node/recommended",
-  ],
-  env: {
-    node: true,
-  },
-  overrides: [
-    {
-      files: ["tests/**/*.js"],
-      env: { mocha: true },
+module.exports = [
+  {
+    parser: "vue-eslint-parser",
+    root: true,
+    extends: [
+      "eslint:recommended",
+      "plugin:eslint-plugin/recommended",
+      "plugin:node/recommended",
+    ],
+    env: {
+      node: true,
     },
-  ],
-};
+    overrides: [
+      {
+        files: ["tests/**/*.js"],
+        env: { mocha: true },
+      },
+    ],
+  },
+];
--- tests/lib/rules/force-nuxt-link-locale.js
+++ tests/lib/rules/force-nuxt-link-locale.js
@@ -11,21 +11,27 @@
 const rule = require("../../../lib/rules/force-nuxt-link-locale"),
   RuleTester = require("eslint").RuleTester;
 
-
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 
-const ruleTester = new RuleTester();
+const ruleTester = new RuleTester({
+  parser: require.resolve("vue-eslint-parser"),
+  parserOptions: { ecmaVersion: 2015 },
+});
 ruleTester.run("force-nuxt-link-locale", rule, {
   valid: [
-    // give me some code that won't trigger a warning
+    {
+      filename: "test.vue",
+      code: `<template><NuxtLinkLocale to="/">Home</NuxtLinkLocale></template>`,
+    },
   ],
 
   invalid: [
     {
-      code: "<NuxtLink to=\"/\">Home</NuxtLink>",
-      errors: [{ message: "Fill me in.", type: "Me too" }],
+      filename: "test.vue",
+      code: `<template><NuxtLink to="/">Home</NuxtLink></template>`,
+      errors: [{ message: "Use NuxtLinkLocale instead of NuxtLink" }],
     },
   ],
 });
